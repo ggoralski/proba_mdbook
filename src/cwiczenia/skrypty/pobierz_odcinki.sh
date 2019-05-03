cd16c706d1d3a6f4f4f784d62533d55bfacadf46
Grzegorz <ggoralski@gmail.com>
2019-05-03 18:20:02 +0200

    ...

--- src/cwiczenia/skrypty/pobierz_odcinki.sh
+++
@@ -1,6 +0,0 @@
-efetch -db nuccore -id NC_001284 -format fasta -seq_start 327890 -seq_stop 328078 > nad7_A_thaliana.fasta
-efetch -db nuccore -id NC_001284 -format fasta -seq_start 329735 -seq_stop 330306 >> nad7_A_thaliana.fasta
-efetch -db nuccore -id NC_001284 -format fasta -seq_start 332945 -seq_stop 333105 >> nad7_A_thaliana.fasta
-efetch -db nuccore -id NC_001284 -format fasta -seq_start 79740 -seq_stop 80132 >> nad7_A_thaliana.fasta
-efetch -db nuccore -id NC_001284 -format fasta -seq_start 81113 -seq_stop 81297 >> nad7_A_thaliana.fasta
-
